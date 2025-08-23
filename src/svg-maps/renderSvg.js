501ff42183d5b3ad0ae92fb6255d73ca4aa07939
Fovir <fovir@disroot.org>
2025-08-23 21:11:18 +0800

    fix: Read empty lines of `.xlsx` files.

--- src/svg-maps/renderSvg.js
+++ src/svg-maps/renderSvg.js
@@ -6,6 +6,9 @@
  */
 
 import XLSX from "xlsx";
+import { SVG, registerWindow } from "@svgdotjs/svg.js";
+import { createSVGWindow } from "svgdom";
+import fs from "fs";
 
 /**
  * @description Read data from `.xlsx` files.
@@ -21,8 +24,11 @@ function readXlsx(fileName) {
     // Get the work sheet.
     const workSheet = workBook.Sheets[sheetName];
 
-    // Read data.
-    let data = XLSX.utils.sheet_to_json(workSheet);
+    // Read data with blank lines.
+    let data = XLSX.utils.sheet_to_json(workSheet, {
+      defval: null,
+      blankrows: true,
+    });
     data = data.slice(0, MAX_ROWS);
     result.push(data);
   }
@@ -30,3 +36,22 @@ function readXlsx(fileName) {
   console.log(typeof result);
   return result;
 }
+
+function renderSvg() {
+  // Read data.
+  const sourceFileName = "./data.xlsx";
+  const storeData = readXlsx(sourceFileName);
+
+  console.log(storeData);
+
+  // Create a virtual window.
+  const window = createSVGWindow();
+  const document = window.document;
+  registerWindow(window, document);
+
+  const svgWidth = 1000;
+  const svgHeight = 500;
+  const draw = SVG(document.documentElement).size(svgWidth, svgHeight);
+}
+
+renderSvg();
