ea8e0dbd0a555ad9deeec817d85e732837e13b4d
Fovir <fovir@disroot.org>
2025-08-25 14:16:42 +0800

    docs: Add comments.

--- src/app/api/posts/[slug]/[locale]/route.jsx
+++ src/app/api/posts/[slug]/[locale]/route.jsx
@@ -1,6 +1,17 @@
+/**
+ * @file app/api/posts/[slug]/[locale]/route.jsx
+ * @description Provide API to fetch post contents.
+ * @author Fovir
+ * @date 2025-08-25
+ */
+
 import fs from "fs";
 import path from "path";
 
+/**
+ * @description Get post content API.
+ * @returns The post content in JSON format.
+ */
 export async function GET(req, { params }) {
   const { slug, locale } = params;
   const filePath = path.join(
