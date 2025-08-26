3c2182153a0d6f399286061ee214191852c0dcd2
Fovir <fovir@disroot.org>
2025-08-26 17:11:38 +0800

    refactor: Only use the sidebar as a container rather than the detail pages.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,36 +1,17 @@
 /**
  * @file Sidebar.jsx
- * @description Sidebar used to display detail information of stores.
+ * @description Sidebar used to contain other pages.
  * @author Fovir
- * @date 2025-08-25
+ * @date 2025-08-26
  */
 
 "use client";
 
-import { marked } from "marked";
-import { useEffect, useState } from "react";
 import { Drawer, IconButton } from "@mui/material";
 import { CloseRounded } from "@mui/icons-material";
 import { styled } from "@mui/material/styles";
 
-export default function Sidebar({ post, onClose, show }) {
-  const [content, setContent] = useState("");
-
-  useEffect(() => {
-    async function loadMarkdown() {
-      const res = await fetch(`/api/posts/${post.slug}/${post.locale}`);
-      if (!res.ok) {
-        setContent({ content: "<p>Failed</p>" });
-        return;
-      }
-
-      const data = await res.json();
-      setContent(marked(data.content));
-    }
-
-    loadMarkdown();
-  }, [post]);
-
+export default function Sidebar({ onClose, show, children }) {
   const DrawerHeader = styled("div")(({ theme }) => ({
     display: "flex",
     alignItems: "center",
@@ -48,7 +29,7 @@ export default function Sidebar({ post, onClose, show }) {
           <CloseRounded />
         </IconButton>
       </DrawerHeader>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+      {children}
     </Drawer>
   );
 }
