38fdd94e9a5fc5273b23b9e785c842e0d3b451e2
Fovir <fovir@disroot.org>
2025-08-27 20:24:17 +0800

    feat: Use the catppuccin theme.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,19 @@
+"use client";
+
 import "../styles/global.css";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "../styles/materialUiTheme";
+import { CssBaseline } from "@mui/material";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
