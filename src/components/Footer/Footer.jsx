abb627f7c82ba286e74ab379bdb72133d5348db2
Fovir <fovir@disroot.org>
2025-09-02 16:54:56 +0800

    feat: Add contact and email information.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -23,37 +23,67 @@ export default function Footer() {
           height: "8vh",
           zIndex: "0",
           background: catppuccinMochaColors.mantle,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "1.4rem",
         }}
       >
-        <Typography
-          sx={{
-            height: "100%",
-            display: "flex",
-            flexDirection: "row",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-            fontSize: "1.4rem",
-          }}
-        >
+        <MyTypography>
           Copyright &copy; 2025 XMUM Map | Built by Mr.Oui &&nbsp;
-          <Link
-            href="https://www.fovir.fyi"
-            sx={{
-              color: catppuccinMochaColors.text,
-              textDecoration: "underline",
-              "&:hover": {
-                color: catppuccinMochaColors.subtext0,
-              },
-              "&:visited": {
-                color: "inherit",
-              },
-            }}
-          >
-            Fovir
-          </Link>
-        </Typography>
+          <MyLink href={"https://www.fovir.fyi"}>Fovir</MyLink>
+        </MyTypography>
+        <MyTypography>Contact: 123456790</MyTypography>
+        <MyTypography>
+          Email:&nbsp;
+          <MyLink href="mailto:example@example.com">
+            example@example.com
+          </MyLink>
+        </MyTypography>
       </Box>
+    </>
+  );
+}
+
+function MyLink({ children, href }) {
+  return (
+    <>
+      <Link
+        href={href}
+        sx={{
+          color: catppuccinMochaColors.text,
+          textDecoration: "underline",
+          "&:hover": {
+            color: catppuccinMochaColors.subtext0,
+          },
+          "&:visited": {
+            color: "inherit",
+          },
+        }}
+      >
+        {children}
+      </Link>
+    </>
+  );
+}
+
+function MyTypography({ children }) {
+  return (
+    <>
+      <Typography
+        sx={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "inherit",
+        }}
+      >
+        {children}
+      </Typography>
     </>
   );
 }
