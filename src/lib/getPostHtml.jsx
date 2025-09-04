ba3deff318ec02d673d5012b74a580cc4ced7bfd
Fovir <fovir@disroot.org>
2025-09-04 16:32:40 +0800

    docs: Add comments.

--- src/lib/getPostHtml.jsx
+++ src/lib/getPostHtml.jsx
@@ -1,5 +1,18 @@
+/**
+ * @file getPostHtml.jsx
+ * @description Get post in `.html` format by `slug` and `locale`.
+ * @author Fovir
+ * @date 2025-09-04
+ */
+
 "use client";
 
+/**
+ * @description Fetch post in html format via API.
+ * @param {string} slug Slug name of the post.
+ * @param {string} locale Current locale.
+ * @returns {string} Html content in string format.
+ */
 export async function getPostHtml(slug, locale) {
   let res = await fetch(`/api/posts/${slug}/${locale}`);
   if (!res.ok) {
