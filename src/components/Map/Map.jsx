b0f3b247357ba93d896b99203daa2de830b71b3a
Fovir <fovir@disroot.org>
2025-09-05 12:38:48 +0800

    docs: Update the edit time.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,7 +2,7 @@
  * @file Map.jsx
  * @description Create the `Map` component to contain all maps. And use `react-zoom-pan-pinch` to enable users to move and zoom maps.
  * @author Fovir
- * @date 2025-08-30
+ * @date 2025-09-05
  */
 
 "use client";
