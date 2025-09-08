d177409dc4875dcb06f37d647431f1614b7350b9
Fovir <fovir@disroot.org>
2025-09-08 20:04:04 +0800

    fix: Fix the update time.

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ const xmumConfig = {
     lastUpdateTime: {
       zh: "上次更新时间：",
       en: "Last update time: ",
-      time: "19 Sept 2025",
+      time: "08 Sept 2025",
     },
   },
   language: {
