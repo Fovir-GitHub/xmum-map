ba1294640d452e97ac4595b21f6e91c85167e73d
Fovir <fovir@disroot.org>
2025-09-09 20:30:04 +0800

    feat: Add loading animation for detail pages.

--- src/components/DetailPage/DetailPage.jsx
+++ src/components/DetailPage/DetailPage.jsx
@@ -2,7 +2,7 @@
  * @file DetailPage.jsx
  * @description Create the `DetailPage` component, which is the container of detail pages. The component fetches post html via API.
  * @author Fovir
- * @date 2025-09-05
+ * @date 2025-09-09
  */
 
 "use client";
@@ -10,19 +10,36 @@
 import { useEffect, useState } from "react";
 import { getMarkdownHtml } from "../../lib/getMarkdownHtml";
 import ImageSlide from "../ImageSlide/ImageSlide";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function DetailPage({ slug, locale }) {
   // Post html.
   const [postHtml, setPostHtml] = useState("");
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    getMarkdownHtml(slug, locale).then((html) => {
+    const fetchPost = async () => {
+      const html = await getMarkdownHtml(slug, locale);
       setPostHtml(html);
-    });
+      setLoading(false);
+    };
+    fetchPost();
   }, [slug, locale]);
 
   return (
     <article className="markdown">
+      {loading && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          <CircularProgress />
+        </div>
+      )}
       <div
         dangerouslySetInnerHTML={{
           __html: postHtml,
