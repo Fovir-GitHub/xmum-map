444802cbb7816cebeba03f306348f02def811b19
Fovir <fovir@disroot.org>
2025-09-20 23:58:45 +0800

    docs: Add comments.

--- src/lib/isValidLocale.jsx
+++ src/lib/isValidLocale.jsx
@@ -1,3 +1,14 @@
+/**
+ * @file isValidLocale.jsx
+ * @author Fovir
+ * @since 2025-09-20
+ */
+
+/**
+ * Check whether the locale is valid.
+ *
+ * @param {string} locale The locale to be checked.
+ */
 export function isValidLocale(locale) {
   return locale && (locale === "zh" || locale === "en");
 }
