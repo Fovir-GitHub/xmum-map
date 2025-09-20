591be966af3b5821d37741c335f2dce8790149b4
Fovir <fovir@disroot.org>
2025-09-20 23:35:48 +0800

    docs: Add comments.

--- src/hooks/useSelectedPost.jsx
+++ src/hooks/useSelectedPost.jsx
@@ -1,11 +1,21 @@
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+/**
+ * Hook of setting selected post.
+ *
+ * @param {string} locale Current locale.
+ *
+ * @returns `[selectedPost, setSelectedPost]`
+ *  - `selectedPost` Current selected post.
+ *  - `setSelectedPost` Function used to set post.
+ */
 export function useSelectedPost(locale) {
   const [selectedPost, setSelectedPost] = useState(null);
   const router = useRouter();
   const pathname = usePathname();
 
+  // Update selected post when hash tag changes.
   useEffect(() => {
     function handleHashChange() {
       const hash = window.location.hash.slice(1);
@@ -27,6 +37,7 @@ export function useSelectedPost(locale) {
       window.removeEventListener("hashchange", handleHashChange);
   }, []);
 
+  // Update hash tag when selected post changes.
   useEffect(() => {
     if (!selectedPost) {
       return;
