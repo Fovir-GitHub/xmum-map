54fb0dff2d347c3b8b5b0bffee0bd49021b7da61
Fovir <fovir@disroot.org>
2025-09-21 17:00:57 +0800

    feat: Add map ref.

--- src/components/Ly3Map/BodySection.jsx
+++ src/components/Ly3Map/BodySection.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import ToolZone from "../ToolZone/ToolZone";
+import Map from "../Map/Map";
 
 export default function BodySection({ locale, setLocale }) {
   const [layer, setLayer] = useState(0);
   const layerRange = 3;
+  const transfromRef = useRef(null);
 
   return (
     <>
@@ -13,7 +15,10 @@ export default function BodySection({ locale, setLocale }) {
         layer={layer}
         setLayer={setLayer}
         layerRange={layerRange}
+        transformRef={transfromRef}
       />
+
+      <Map transformRef={transfromRef}></Map>
     </>
   );
 }
