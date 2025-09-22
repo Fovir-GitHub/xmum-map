c2a3d2ca773edc44103b9d01a65259cf831fe87c
Fovir <fovir@disroot.org>
2025-09-22 17:48:17 +0800

    feat: Add the left row of stores.

--- src/components/Ly3Map/Ly3SecondFloor.jsx
+++ src/components/Ly3Map/Ly3SecondFloor.jsx
@@ -3,6 +3,10 @@
 import DrawAMap from "../DrawAMap/DrawAMap";
 
 export default function Ly3SecondFloor({ storeData, locale }) {
+  const topRowStart = 0,
+    topRowEnd = 8;
+  const leftRowStart = topRowEnd,
+    leftRowEnd = 29;
   return (
     <div>
       <img src="maps/LY3_2F.svg" width={10000} height={10000} />
@@ -17,7 +21,14 @@ export default function Ly3SecondFloor({ storeData, locale }) {
           left: 0,
         }}
       >
-        <TopRow storeData={storeData} locale={locale} />
+        <TopRow
+          storeData={storeData.slice(topRowStart, topRowEnd)}
+          locale={locale}
+        />
+        <LeftRow
+          storeData={storeData.slice(leftRowStart, leftRowEnd)}
+          locale={locale}
+        />
       </div>
     </div>
   );
@@ -50,6 +61,37 @@ function TopRow({ storeData, locale }) {
           height: 400,
           width: 350,
           fontSize: "4rem",
+        })}
+      </svg>
+    </div>
+  );
+}
+
+function LeftRow({ storeData, locale }) {
+  const rowIndexLimit = 40;
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        top: 600,
+        left: -100,
+      }}
+    >
+      <svg
+        width={"100%"}
+        height={"100%"}
+        transform="rotate(-90, 100, 50)"
+      >
+        {DrawAMap({
+          stores: storeData,
+          rowIndexLimit: rowIndexLimit,
+          limit: rowIndexLimit,
+          locale: locale,
+          fontSize: "4rem",
+          height: 400,
+          width: 300,
         })}
       </svg>
     </div>
