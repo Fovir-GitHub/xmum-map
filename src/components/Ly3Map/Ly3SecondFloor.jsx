d1ca9cee3363da0c7e4352cd75d958f9540a796e
Fovir <fovir@disroot.org>
2025-09-22 15:10:04 +0800

    feat: Add stores of the top row.

--- src/components/Ly3Map/Ly3SecondFloor.jsx
+++ src/components/Ly3Map/Ly3SecondFloor.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import DrawAMap from "../DrawAMap/DrawAMap";
+
 export default function Ly3SecondFloor({ storeData, locale }) {
   return (
     <div>
@@ -14,7 +16,42 @@ export default function Ly3SecondFloor({ storeData, locale }) {
           top: 0,
           left: 0,
         }}
-      ></div>
+      >
+        <TopRow storeData={storeData} locale={locale} />
+      </div>
+    </div>
+  );
+}
+
+function TopRow({ storeData, locale }) {
+  const rowIndexLimit = 8;
+  const limit = 8;
+
+  const top = 1750;
+  const left = 200;
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        top: top,
+        left: left,
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <svg width={"100%"} height={"100%"}>
+        {DrawAMap({
+          stores: storeData,
+          rowIndexLimit: rowIndexLimit,
+          limit: limit,
+          locale: locale,
+          handleStoreBlockClick: () => console.log("Clicked"),
+          height: 400,
+          width: 350,
+          fontSize: "4rem",
+        })}
+      </svg>
     </div>
   );
 }
