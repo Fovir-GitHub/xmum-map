799df9bff28878bee50bf926f074949865251b39
Fovir <fovir@disroot.org>
2025-09-23 14:25:48 +0800

    docs: Add comments.

--- src/lib/readData.jsx
+++ src/lib/readData.jsx
@@ -8,6 +8,11 @@
 import fs from "fs";
 import path from "path";
 
+/**
+ * Read data from `src/data/${fileName}` file.
+ *
+ * The `fileName` includes extension name.
+ */
 export function readData(fileName) {
   const dataFile = path.join(process.cwd(), `src/data/${fileName}`);
   const fileContent = fs.readFileSync(dataFile);
