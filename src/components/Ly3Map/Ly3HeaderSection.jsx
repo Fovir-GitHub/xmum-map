05a3360c1b9906a6a4987dffe46207548229d813
Fovir <fovir@disroot.org>
2025-09-23 16:00:50 +0800

    docs: Add comments.

--- src/components/Ly3Map/Ly3HeaderSection.jsx
+++ src/components/Ly3Map/Ly3HeaderSection.jsx
@@ -1,5 +1,17 @@
+/**
+ * @file Ly3HeaderSection.jsx
+ * @description Header section of LY3 map.
+ * @author Fovir
+ * @since 2025-09-23
+ */
+
 import Announcement from "../Announcement/Announcement";
 
+/**
+ * Wrapper of header section of LY3 map, which includes an `Announcement` component.
+ *
+ * @param {string} locale Current locale.
+ */
 export default function Ly3HeaderSection({ locale }) {
   return (
     <>
--- src/components/Ly3Map/Ly3Map.jsx
+++ src/components/Ly3Map/Ly3Map.jsx
@@ -1,3 +1,10 @@
+/**
+ * @file Ly3Map.jsx
+ * @description Wrapper of LY3 map.
+ * @author Fovir
+ * @since 2025-09-21
+ */
+
 "use client";
 
 import { ThemeProvider } from "@emotion/react";
@@ -8,6 +15,11 @@ import Footer from "../Footer/Footer";
 import Ly3BodySection from "./Ly3BodySection";
 import Ly3HeaderSection from "./Ly3HeaderSection";
 
+/**
+ * Map of LY3.
+ *
+ * @param {object[][]} ly3Data Data of LY3 stores.
+ */
 export default function Ly3Map({ ly3Data }) {
   const [locale, setLocale] = useLocale();
 
