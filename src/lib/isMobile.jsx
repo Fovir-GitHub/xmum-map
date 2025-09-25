a22c2334402815e290bf84148cc5ffceae7347be
Fovir <fovir@disroot.org>
2025-09-25 20:39:38 +0800

    docs: Add comments.

--- src/lib/isMobile.jsx
+++ src/lib/isMobile.jsx
@@ -8,6 +8,13 @@
 import { useMediaQuery } from "@mui/material";
 import { theme } from "../styles/materialUiTheme";
 
+/**
+ * Determine whether current device is a mobile device.
+ *
+ * @returns {boolean}
+ *  - `true` Current device is a mobile device.
+ *  - `false` Current device is not a mobile device.
+ */
 export function isMobile() {
   return useMediaQuery(theme.breakpoints.down("sm"));
 }
