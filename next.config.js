7fa41979b2ca4f3f41b7e6dc17b9898c7d7a2a33
Fovir <fovir@disroot.org>
2025-09-26 19:01:29 +0800

    chore: Use ES module syntax.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+/** @type {import("next").NextConfig} */
 const nextConfig = {
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  webpack(config) {
+    config.externals = config.externals || [];
+    config.externals.push(/.*\.pnpm-store.*/);
+    return config;
+  },
   outputFileTracingExcludes: { "/api/docs": ["./.next/cache/**/*"] },
 };
+
+export default nextConfig;
