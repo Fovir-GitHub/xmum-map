6d31995351cb79d73416d50cb1157ad56e100deb
Fovir <fovir@disroot.org>
2025-09-27 16:20:39 +0800

    feat: Import header section of A3.

---
+++ src/components/A3Map/A3Map.jsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from "@emotion/react";
+import { useLocale } from "../../hooks/useLocale";
+import { theme } from "../../styles/materialUiTheme";
+import { CssBaseline } from "@mui/material";
+import A3HeaderSection from "./A3HeaderSection";
+
+export default function A3Map({ a3Data }) {
+  const [locale, setLocale] = useLocale();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <A3HeaderSection locale={locale} />
+    </ThemeProvider>
+  );
+}
