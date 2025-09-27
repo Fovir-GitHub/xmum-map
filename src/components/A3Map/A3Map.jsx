dc777f0d49501469694d4c414f9515d40d1898da
Fovir <fovir@disroot.org>
2025-09-27 16:52:33 +0800

    docs: Add comments.

--- src/components/A3Map/A3Map.jsx
+++ src/components/A3Map/A3Map.jsx
@@ -1,3 +1,10 @@
+/**
+ * @file src/components/A3Map/A3Map.jsx
+ * @description A3 map.
+ * @author Fovir
+ * @since 2025-09-27
+ */
+
 import { ThemeProvider } from "@emotion/react";
 import { useLocale } from "../../hooks/useLocale";
 import { theme } from "../../styles/materialUiTheme";
@@ -6,7 +13,19 @@ import A3HeaderSection from "./A3HeaderSection";
 import Footer from "../Footer/Footer";
 import A3BodySection from "./A3BodySection";
 
-export default function A3Map({ a3Data }) {
+/**
+ * @typedef {object} A3MapProps
+ *
+ * @property {object[][]} a3Data Data of A3 areas or stores.
+ */
+
+/**
+ * Map of A3.
+ */
+export default function A3Map(
+  /** @type {A3MapProps} */
+  { a3Data },
+) {
   const [locale, setLocale] = useLocale();
 
   return (
