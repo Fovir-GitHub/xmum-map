3d3cb5c8dba2bcb686c21c78d4c8436ce18cfbb1
Fovir <fovir@disroot.org>
2025-09-27 16:21:48 +0800

    feat: Add footer to A3 map.

--- src/components/A3Map/A3Map.jsx
+++ src/components/A3Map/A3Map.jsx
@@ -3,6 +3,7 @@ import { useLocale } from "../../hooks/useLocale";
 import { theme } from "../../styles/materialUiTheme";
 import { CssBaseline } from "@mui/material";
 import A3HeaderSection from "./A3HeaderSection";
+import Footer from "../Footer/Footer";
 
 export default function A3Map({ a3Data }) {
   const [locale, setLocale] = useLocale();
@@ -12,6 +13,8 @@ export default function A3Map({ a3Data }) {
       <CssBaseline />
 
       <A3HeaderSection locale={locale} />
+
+      <Footer locale={locale} />
     </ThemeProvider>
   );
 }
