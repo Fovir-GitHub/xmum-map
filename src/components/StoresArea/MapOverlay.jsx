e950224bbf751cad21f1d4b0fcd4a284e765e488
Fovir <fovir@disroot.org>
2025-09-27 18:07:06 +0800

    fix: Remove the `svg` tag inside `MapOverlay` component.

--- src/components/StoresArea/MapOverlay.jsx
+++ src/components/StoresArea/MapOverlay.jsx
@@ -29,14 +29,7 @@ export default function MapOverlay(
         left: 0,
       }}
     >
-      <svg
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        {children}
-      </svg>
+      {children}
     </div>
   );
 }
