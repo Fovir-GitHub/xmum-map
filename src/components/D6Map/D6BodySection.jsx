74cd214b78c450d50cba6ba111d63e9c8a242aec
Fovir <fovir@disroot.org>
2025-10-03 08:55:15 +0800

    docs: Add comments.

--- src/components/D6Map/D6BodySection.jsx
+++ src/components/D6Map/D6BodySection.jsx
@@ -1,3 +1,10 @@
+/**
+ * @file src/components/D6Map/D6BodySection.jsx
+ * @description Body section of D6.
+ * @author Fovir
+ * @since 2025-10-03
+ */
+
 import { usePathname, useRouter } from "next/navigation";
 import { useRef, useState } from "react";
 import { useSelectedPost } from "../../hooks/useSelectedPost";
@@ -7,7 +14,21 @@ import Sidebar from "../Sidebar/Sidebar";
 import DetailPage from "../DetailPage/DetailPage";
 import Map from "../Map/Map";
 
-export default function D6BodySection({ locale, setLocale, d6Data }) {
+/**
+ * @typedef {object} D6BodySectionProps
+ *
+ * @property {string} locale Current locale.
+ * @property {Function} setLocale Function used to change the value of `locale`.
+ * @property {object[][]} d6Data Store data of D6.
+ */
+
+/**
+ * Body section of D6.
+ */
+export default function D6BodySection(
+  /** @type {D6BodySectionProps} */
+  { locale, setLocale, d6Data },
+) {
   const [layer, setLayer] = useState(0);
   const layerRange = 3;
   const layerStart = 0;
