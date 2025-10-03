1739e1b8f8ca21f0cbe98487ad59d4239e491096
Fovir <fovir@disroot.org>
2025-10-03 08:24:53 +0800

    feat: Add page of D6.

---
+++ src/app/d6/page.jsx
@@ -0,0 +1,15 @@
+/**
+ * @file src/app/d6/page.jsx
+ * @description Map of D6.
+ * @author Fovir
+ * @since 2025-10-03
+ */
+
+import { readData } from "../../lib/readData";
+
+/**
+ * Page of D6 map.
+ */
+export default function Ly3() {
+  const d6Data = readData("d6.json");
+}
