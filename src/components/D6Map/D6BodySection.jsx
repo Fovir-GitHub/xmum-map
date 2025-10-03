15ccc875a947fae204891a672bb563bc30ee8df5
Fovir <fovir@disroot.org>
2025-10-03 08:52:38 +0800

    feat: Add basic structure of D6 body.

---
+++ src/components/D6Map/D6BodySection.jsx
@@ -0,0 +1,50 @@
+import { usePathname, useRouter } from "next/navigation";
+import { useRef, useState } from "react";
+import { useSelectedPost } from "../../hooks/useSelectedPost";
+import ToolZone from "../ToolZone/ToolZone";
+import { getIgnoreLayers } from "../../lib/getIgnoreLayers";
+import Sidebar from "../Sidebar/Sidebar";
+import DetailPage from "../DetailPage/DetailPage";
+import Map from "../Map/Map";
+
+export default function D6BodySection({ locale, setLocale, d6Data }) {
+  const [layer, setLayer] = useState(0);
+  const layerRange = 3;
+  const layerStart = 0;
+  const transformRef = useRef(null);
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const [selectedPost, setSelectedPost] = useSelectedPost(locale);
+
+  const maps = [null, null, null];
+
+  return (
+    <>
+      <ToolZone
+        locale={locale}
+        setLocale={setLocale}
+        layer={layer}
+        setLayer={setLayer}
+        layerRange={layerRange}
+        transformRef={transformRef}
+        layerIgnore={getIgnoreLayers(layerStart, maps)}
+      />
+
+      <Sidebar
+        selectedPost={selectedPost}
+        setSelectedPost={setSelectedPost}
+        router={router}
+        pathname={pathname}
+      >
+        <DetailPage
+          slug={selectedPost?.slug || "404"}
+          locale={locale}
+        />
+      </Sidebar>
+
+      <Map transformRef={transformRef}>{maps[layerStart]}</Map>
+    </>
+  );
+}
