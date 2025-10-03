ba8d52c90e792b945e0b9675c7a2a5dfc8c61744
Fovir <fovir@disroot.org>
2025-10-03 08:30:24 +0800

    docs: Add comments.

--- src/components/D6Map/D6Map.jsx
+++ src/components/D6Map/D6Map.jsx
@@ -1,6 +1,25 @@
+/**
+ * @file src/components/D6Map/D6Map.jsx
+ * @description Wrapper of D6 map.
+ * @author Fovir
+ * @since 2025-10-03
+ */
+
 import { useLocale } from "../../hooks/useLocale";
 
-export default function D6Map({ d6Data }) {
+/**
+ * @typedef {object} {D6MapProps}
+ *
+ * @property {object[][]} d6Data Store data of D6.
+ */
+
+/**
+ * Map of D6.
+ */
+export default function D6Map(
+  /** @type {D6MapProps} */
+  { d6Data },
+) {
   const [locale, setLocale] = useLocale();
 
   return (
