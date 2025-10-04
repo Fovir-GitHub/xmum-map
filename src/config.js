bdb0ce437350eff4721cd7ab0c8a37a0f35db451
o-u-i <a812354120@gmail.com>
2025-10-04 10:37:14 +0800

    chore: change updated time

--- src/config.js
+++ src/config.js
@@ -44,7 +44,7 @@ const xmumConfig = {
     lastUpdateTime: {
       zh: "⏰ 上次更新时间：",
       en: "⏰ Last update time: ",
-      time: "02 Oct 2025",
+      time: "04 Oct 2025",
     },
   },
   language: {
